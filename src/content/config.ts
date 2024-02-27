320f87985087103f89e88480fb36e0a4fb73ac9e
rushilsheth <sheth.rushil@gmail.com>
2024-02-27 14:19:58 -0800

    header works for blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,16 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      pubDate: z
-        .string()
-        .or(z.date())
-        .transform((val) => new Date(val)),
-      updatedDate: z
-        .string()
-        .optional()
-        .transform((str) => (str ? new Date(str) : undefined)),
-      heroImage: image().optional(),
-      heroImageCaption: z.string().optional(),
-      draft: z.boolean().optional(),
-    }),
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
 
 export const collections = { blog };
