4a065c796df2580acb6e3e2775929b8ab94113a4
rushilsheth <sheth.rushil@gmail.com>
2024-03-26 09:09:36 -0700

    new blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rushilsheth.com',
 	integrations: [mdx({drafts: true,}), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+	}
 });
